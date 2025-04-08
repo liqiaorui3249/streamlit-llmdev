c088463ffc00b50a7cd99d730cef686fbd5d1260
liqiaorui3249 <liqiaorui@163.com>
2025-04-08 10:36:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,103 @@
+import datetime
 import streamlit as st
+st.title("hello llm dev")
+st.divider()
+st.write("I am fine")
+st.divider()
+st.info("Hello lqr")
+st.divider()
+st.header("Hello everyone")
+st.divider()
+st.markdown("Hello **world**!")
+st.divider()
+st.subheader("This is a subheader")
+st.badge("New")
+st.caption("This is written small caption text")
+st.divider()
+st.code("a = 1234")
+st.latex("\int a x^2 \,dx")
+st.divider()
+st.html("<p>Foo bar.</p>")
+st.button("Reset", type="primary")
+st.divider()
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+st.divider()
+st.write("图片控制")
+enable = st.checkbox("Enable camera")
+picture = st.camera_input("Take a picture", disabled=not enable)
 
-#聊天机器人页面，可视化展示内容和大模型的聊天记录
+if picture:
+    st.image(picture)
 
-#构建和大模型的聊天chain
 
-#页面的左半部分展示聊天内容，右半部分展示聊天记录
+st.divider()
+st.write("时间控件")
 
-from pydantic import BaseModel
+today = datetime.datetime.now()
+next_year = today.year + 1
+jan_1 = datetime.date(next_year, 1, 1)
+dec_31 = datetime.date(next_year, 12, 31)
 
-#通义大模型
-from langchain_community.chat_models.tongyi import ChatTongyi 
-#提示词模型
-from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
-#输出的解析器
-from langchain_core.output_parsers import StrOutputParser
-#定义大模型
-from langchain.schema import AIMessage,HumanMessage
-
-model=ChatTongyi(model_name='qwen-max',streaming=True)
-mermory_key='history'
-prompt=ChatPromptTemplate.from_messages(
-	[
-		MessagesPlaceholder(variable_name=mermory_key),
-		('human','{input}')
-	]
+d = st.date_input(
+    "Select your vacation for next year",
+    (jan_1, datetime.date(next_year, 1, 7)),
+    jan_1,
+    dec_31,
+    format="MM.DD.YYYY",
 )
+d
+st.divider()
+st.write("输入框")
+name=st.text_input("First name")
+st.write(f"Hello{name}")
 
-class Message(BaseModel):	
-	content:str
-	role:str
+st.divider()
+st.write("选择框")
+choice=st.number_input("pick a number,0,10")
 
-if "messages" not in st.session_state:
-	st.session_state.messages = []
-	
-def to_message_place_holder(messages):
-	return [
-		AIMessage(content=message['content']) if message['role']=="ai" else HumanMessage(content=message['content']) 
-		for message in messages
-	]
-	
-chain = {
-	'input':lambda x: x['input'],
-	'history':lambda x:to_message_place_holder(x['messages'])
-} | prompt | model | StrOutputParser()
+st.divider()
+st.write("多行框")
+text=st.text_area("text to translate")
 
-#构建页面
-st.columns([0.7,0.3])
-left,right = st.columns([0.7,0.3])
+st.divider()
+st.write("上传")
+data=st.file_uploader("Upload a csv")
 
-with left:
-    #聊天内容展示
-	container=st.container()
-	with container:
-		for message in st.session_state.messages:
-			with st.chat_message(message['role']):
-			     st.write(message['content'])
-	
-	#接收用户的输入，存放在sessin_state中
-	prompt = st.chat_input("您好，请问有什么可以帮助到您的吗？")
-	if prompt:
-		st.session_state.messages.append(Message(content=prompt,role="human").model_dump())
-		with container:
-			with st.chat_message("human"):
-			     st.write(prompt)
-	#获取大模型的返回，并展示
-	with container:
-		response = st.write_stream(chain.stream({"input":prompt,'messages':st.session_state.messages}))
-		st.session_state.messages.append(Message(content=response,role="ai").model_dump())
-		
-	
 
-with right:
-	#聊天记录	
-	st.json(st.session_state.messages)
+st.sidebar.write("This lives in the sidebar")
+st.sidebar.divider()
+st.sidebar.button("click me!")
+
+st.divider()
+st.write("扩展器")
+with st.expander("open to see more"):
+     st.write("this is more content")
+     st.write("this is another content")
+     st.image("https://static.streamlit.io/examples/dice.jpg")
+
+st.divider()
+st.write("标签页")
+tab1,tab2=st.tabs(["tab1","tab2"])
+tab1.write("this is tab1")
+tab2.write("this is tab2")
+
+st.divider()
+st.write("聊天输入")
+myname=st.text_input("First_name")
+if name:
+    st.write(f"Hello {myname}")
+prompt=st.chat_input("say something")
+if prompt:
+    st.write(f"the user has sent:{prompt}")
+
+st.divider()
+st.write("聊天消息")
+import numpy as np
+with st.chat_message("user"):
+     st.write("hello")
+     st.line_chart(np.random.randn(30,3))
+with st.chat_message("ai"):
+     st.write("I am an AI assistant")
